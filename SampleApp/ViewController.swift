4e11b7e23bae8b61b1281cf81d10deae38b3c59a
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2014-06-23 19:41:00 -0700

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by vs on 6/23/14.
+//  Copyright (c) 2014 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
